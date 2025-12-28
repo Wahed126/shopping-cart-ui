5f8401ecff172168c9ce44188d3c8db9f5d45dec
Abdul Wahed <waheedmohammadi126@gmail.com>
2025-12-29 00:22:37 +0100

    feat: add Header component for displaying the shopping cart and item count

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
+import Header from "./components/Header";
 import ProductList from "./components/ProductList";
-
 const App = () => {
   return (
-    <div className="min-h-screen bg-gray-100 p-6">
-      <h1 className="text-3xl font-bold mb-6">Porduct Catalog</h1>
-
-      <ProductList />
-    </div>
+    <>
+      <Header />
+      <div className="min-h-screen bg-gray-100 p-6">
+        <h1 className="text-3xl font-bold mb-6">Porduct Catalog</h1>
+        <ProductList />
+      </div>
+    </>
   );
 };
 
