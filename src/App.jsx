fbb8253a18f1f5090ed3bf21963bc2e0606c7ea1
Abdul Wahed <waheedmohammadi126@gmail.com>
2025-12-28 14:19:18 +0100

    feat: add json-server for mock API and implement product fetching in App component
    - Added a script to run json-server for serving mock product data.
    - Updated App component to fetch products from the new json-server endpoint.
    - Removed old db.json file and added a new one with product data.
    - Improved loading and error handling in the product fetching logic.

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,52 @@
+import { useState, useEffect } from "react";
 const App = () => {
-  return <div>Shopping Cart UI.</div>;
+  const [products, setProducts] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    const fetchProducts = async () => {
+      try {
+        const responce = await fetch("http://localhost:8000/products");
+        if (!responce.ok) throw new Error("Faild to fetch products");
+        const data = await responce.json();
+        console.log(data);
+        setProducts(data);
+      } catch (err) {
+        setError(err.message);
+      } finally {
+        setLoading(false);
+      }
+    };
+    fetchProducts();
+  }, []);
+  return (
+    <div className="min-h-screen bg-gray-100 p-6">
+      <h1 className="text-3xl font-bold mb-6">Porduct Catalog</h1>
+
+      {loading && <p>Loading....</p>}
+      {error && <div className="error">{error}</div>}
+
+      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-6">
+        {products.map((product) => (
+          <div
+            key={product.id}
+            className="bg-white rouded-lg shadow p-4 flex flex-col"
+          >
+            <img
+              src={product.image}
+              alt={product.name}
+              className="h-40 object-cover rounded mb-4"
+            />
+
+            <h2 className="text-xl font-semibold">{product.name}</h2>
+            <p className="text-gray-500 text-sm mb-2">{product.description}</p>
+            <p className="font-bold text-lg">${product.price.toFixed(2)}</p>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export default App;
