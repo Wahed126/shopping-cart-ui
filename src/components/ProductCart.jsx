2c33486273fee5208606a1002f682bb9bdc8a9f2
Abdul Wahed <waheedmohammadi126@gmail.com>
2025-12-28 23:59:12 +0100

    feat: implement CartContext for managing cart state and add addToCart function.

--- src/components/ProductCart.jsx
+++ src/components/ProductCart.jsx
@@ -1,4 +1,6 @@
+import { useCart } from "../context/CartContext";
 const ProductCart = ({ product }) => {
+  const { addToCart } = useCart();
   return (
     <div className="bg-white rouded-lg shadow p-4 flex flex-col">
       <img
@@ -10,6 +12,13 @@ const ProductCart = ({ product }) => {
       <h2 className="text-xl font-semibold">{product.name}</h2>
       <p className="text-gray-500 text-sm mb-2">{product.description}</p>
       <p className="font-bold text-lg">${product.price.toFixed(2)}</p>
+
+      <button
+        onClick={() => addToCart(product)}
+        className="bg-blue-600 text-white mt-3 px-4 py-2 rounded hover:bg-blue-700 transition"
+      >
+        Add to Cart
+      </button>
     </div>
   );
 };
